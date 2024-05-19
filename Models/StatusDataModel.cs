59fc88fbc75014b4746c02a910e2e33e26f9fdeb
Oskar Kubala <90306268+Texas02@users.noreply.github.com>
2024-05-20 00:12:17 +0200

    Add files via upload

---
+++ Models/StatusDataModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChataAPP.Models
+{
+   public class StatusDataModel
+    {
+        public string ContactName { get; set; }
+        public Uri ContactPhoto { get; set; }
+
+        public string StatusMessage{ get; set; }
+
+        public Uri StatusImage { get; set; }
+
+        public bool IsMeAddStatus { get; set; }
+
+    }
+}
---
+++ ViewModels/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using ChataAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChataAPP.ViewModels
+{
+    public  class ViewModel
+    {
+       public ObservableCollection<StatusDataModel> statusThumbsCollection;
+        public ViewModel()
+        {
+            statusThumbsCollection = new ObservableCollection<StatusDataModel>()
+            {
+                new StatusDataModel()
+                {
+                    IsMeAddStatus = true
+                },
+                new StatusDataModel()
+                {
+                    ContactName = "Rysiu",
+                    ContactPhoto = new Uri("/Assets/1.png", UriKind.RelativeOrAbsolute),
+                    StatusImage = new Uri("/Assets/5.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus = false
+                },
+                new StatusDataModel()
+                {
+                    ContactName = "Rysiu",
+                    ContactPhoto = new Uri("/Assets/1.png", UriKind.RelativeOrAbsolute),
+                    StatusImage = new Uri("/Assets/5.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus = false
+                },
+                new StatusDataModel()
+                {
+                    ContactName = "Rysiu",
+                    ContactPhoto = new Uri("/Assets/1.png", UriKind.RelativeOrAbsolute),
+                    StatusImage = new Uri("/Assets/5.jpg", UriKind.RelativeOrAbsolute),
+                    IsMeAddStatus = false
+                },
+                };
+        }
+    }
+}
